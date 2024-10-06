575633e1a036d7dc3b6f577551f654006369f055
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-10-06 00:50:58 -0300

    integrando Front e Back

--- src/app/(pagina)/page.tsx
+++ src/app/(pagina)/page.tsx
@@ -1,7 +1,9 @@
+'use client';
 import ProdutoItem from '@/components/produto/ProdutoItem';
-import { produtos } from '@/core';
+import UseProdutos from '@/data/hooks/useProdutos';
 
 export default function Inicio() {
+  const { produtos } = UseProdutos();
   return (
     <div className="grid grid-cols-4 gap-5 container">
       {produtos.map((produto) => (
